86508fa3ddbb4b251bff683da1b586f65d5920e3
B.Silva <145791081+Bsilvaor@users.noreply.github.com>
2024-02-13 18:34:31 +0100

    Update models.py

--- userJSON/models.py
+++ userJSON/models.py
@@ -4,8 +4,8 @@ class Usuario(models.Model):
     nickname = models.CharField(max_length=255)
     clave = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.nickname
+    #def __str__(self):
+        #return self.nickname
     
     #ESTE DEF PODRÍA LLEVARLO A VIEWS?
 
